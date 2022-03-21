4814dc686735120e11ed52504e623adf929213c4
clyw-kc <95829750+clyw-kc@users.noreply.github.com>
2022-03-21 14:27:49 +0000

    a

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ from starlette.responses import Response
 async def app(scope, receive, send):
     assert scope['type'] == 'http'
     request = Request(scope, receive)
-    content = request.url
+    content = request.url.path
     response = Response(content, media_type='text/plain')
     await response(scope, receive, send)
